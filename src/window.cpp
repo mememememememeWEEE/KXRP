570e3f9749ed34082d1e705c91a63d13c3e1c3c3
mememememememeWEEE <83303201+mememememememeWEEE@users.noreply.github.com>
2023-02-14 23:05:41 +0000

    .

--- src/window.cpp
+++ src/window.cpp
@@ -1,15 +1,18 @@
+#include <string>
+#include "../lib/glfw/glfw3.h"
+#include "kxrp.hpp"
 #include "window.hpp"
 
 using namespace KXRP;
 
-window::window() {
-
+window::window(const unsigned int& width, const unsigned int& height, const std::string& title) {
+    this->InitWindow(width, height, title.c_str());
 }
 
 window::~window() {
 
 }
 
-void window::InitWindow() {
+void window::InitWindow(const unsigned int& width, const unsigned int& height, const char* title) {
 
 }
--- src/window.hpp
+++ src/window.hpp
@@ -1,12 +1,20 @@
 #pragma once
+#include <string>
 
-namespace KXRP {
-    class window {
-        public:
-            window();
-            ~window();
+class window final {
+public:
+    window(const unsigned int& width, const unsigned int& height, const std::string& title);
+    ~window();
 
-        protected:
-            void InitWindow();
-    };
-}
+protected:
+    void InitWindow(const unsigned int& width, const unsigned int& height, const char* title);
+
+    inline unsigned int GetWidth() const {return this->width;}
+    inline unsigned int GetHeight() const {return this->height;}
+    inline std::string GetTitle() const {return this->title;}
+
+protected:
+    unsigned int width;
+    unsigned int height;
+    std::string title;
+};
